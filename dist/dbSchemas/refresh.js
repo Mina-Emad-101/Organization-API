60442fa54949b5baa483de4fc61f27e359aa8edd
Mina Emad <menamanos@gmail.com>
2024-10-25 20:44:18 +0300

    gitignore refresh

--- dist/dbSchemas/refresh.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model } from "mongoose";
-const schema = new Schema({
-    token: {
-        type: Schema.Types.String,
-        required: true,
-    },
-});
-export const RefreshToken = model("RefreshToken", schema);
