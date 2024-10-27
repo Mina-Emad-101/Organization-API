f2299902c26205a85de697f4ef8deceaee88286a
Mina Emad <menamanos@gmail.com>
2024-10-27 17:18:20 +0300

    set all handlers to return the response

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -4,150 +4,139 @@ import { User } from "../dbSchemas/user.js";
 import jwt from "jsonwebtoken";
 import { redis } from "../data-sources.js";
 import type {
-  IRefreshToken,
-  ISignin,
-  ISignup,
+	IRefreshToken,
+	ISignin,
+	ISignup,
 } from "../types/handlers/auth.js";
 
 export async function signup(req: Request<{}, {}, ISignup>, res: Response) {
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
-    res
-      .status(400)
-      .json({ message: "Validation Errors", errors: result.array() });
-    return;
-  }
+	const result = validationResult(req);
+	if (!result.isEmpty()) {
+		return res
+			.status(400)
+			.json({ message: "Validation Errors", errors: result.array() });
+	}
 
-  const { name, email, password } = req.body;
+	const { name, email, password } = req.body;
 
-  const user = new User({
-    name: name,
-    email: email,
-    password: password,
-  });
+	const user = new User({
+		name: name,
+		email: email,
+		password: password,
+	});
 
-  await user.save();
+	await user.save();
 
-  res.json({ message: "Success" });
-  return;
+	return res.json({ message: "Success" });
 }
 
 export async function signin(req: Request<{}, {}, ISignin>, res: Response) {
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
-    res
-      .status(400)
-      .json({ message: "Validation Errors", errors: result.array() });
-    return;
-  }
+	const result = validationResult(req);
+	if (!result.isEmpty()) {
+		return res
+			.status(400)
+			.json({ message: "Validation Errors", errors: result.array() });
+	}
 
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  const user = await User.findOne({ email: email });
-  if (!user) {
-    res.status(404).json({ message: "Email Doesn't Exist" });
-    return;
-  }
+	const user = await User.findOne({ email: email });
+	if (!user) {
+		return res.status(404).json({ message: "Email Doesn't Exist" });
+	}
 
-  if (user.password !== password) {
-    res.status(401).json({ message: "Password Incorrect" });
-    return;
-  }
+	if (user.password !== password) {
+		return res.status(401).json({ message: "Password Incorrect" });
+	}
 
-  const access_token = jwt.sign(
-    { id: user.id },
-    process.env.ACCESS_TOKEN_SECRET as string,
-    { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION as string },
-  );
+	const access_token = jwt.sign(
+		{ id: user.id },
+		process.env.ACCESS_TOKEN_SECRET as string,
+		{ expiresIn: process.env.ACCESS_TOKEN_EXPIRATION as string },
+	);
 
-  const tokenID = Math.random().toString(36).slice(2);
-  const refresh_token = jwt.sign(
-    { id: tokenID, user_id: user.id },
-    process.env.REFRESH_TOKEN_SECRET as string,
-  );
+	const tokenID = Math.random().toString(36).slice(2);
+	const refresh_token = jwt.sign(
+		{ id: tokenID, user_id: user.id },
+		process.env.REFRESH_TOKEN_SECRET as string,
+	);
 
-  await redis.setEx(
-    `${user.id}:${tokenID}`,
-    parseInt(process.env.REFRESH_TOKEN_EXPIRATION as string),
-    refresh_token,
-  );
+	await redis.setEx(
+		`${user.id}:${tokenID}`,
+		parseInt(process.env.REFRESH_TOKEN_EXPIRATION as string),
+		refresh_token,
+	);
 
-  res.json({
-    message: "Success",
-    access_token: access_token,
-    refresh_token: refresh_token,
-  });
-  return;
+	return res.json({
+		message: "Success",
+		access_token: access_token,
+		refresh_token: refresh_token,
+	});
 }
 
 export async function refreshToken(
-  req: Request<{}, {}, IRefreshToken>,
-  res: Response,
+	req: Request<{}, {}, IRefreshToken>,
+	res: Response,
 ) {
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
-    res
-      .status(400)
-      .json({ message: "Validation Errors", errors: result.array() });
-    return;
-  }
+	const result = validationResult(req);
+	if (!result.isEmpty()) {
+		return res
+			.status(400)
+			.json({ message: "Validation Errors", errors: result.array() });
+	}
 
-  const { refresh_token } = req.body;
+	const { refresh_token } = req.body;
 
-  jwt.verify(
-    refresh_token,
-    process.env.REFRESH_TOKEN_SECRET as string,
-    {},
-    async (err: any, token: any) => {
-      if (err) {
-        res.status(400).json({ message: "Invalid Refresh Token" });
-        return;
-      }
+	jwt.verify(
+		refresh_token,
+		process.env.REFRESH_TOKEN_SECRET as string,
+		{},
+		async (err: any, token: any) => {
+			if (err) {
+				return res.status(400).json({ message: "Invalid Refresh Token" });
+			}
 
-      console.log(token);
+			console.log(token);
 
-      const token_exists: boolean =
-        (await redis.exists(`${token.user_id}:${token.id}`)) === 1;
+			const token_exists: boolean =
+				(await redis.exists(`${token.user_id}:${token.id}`)) === 1;
 
-      if (!token_exists) {
-        res.status(400).json({ message: "Expired Refresh Token" });
-        return;
-      }
+			if (!token_exists) {
+				return res.status(400).json({ message: "Expired Refresh Token" });
+			}
 
-      const access_token = jwt.sign(
-        { id: token.user_id },
-        process.env.ACCESS_TOKEN_SECRET as string,
-        { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION as string },
-      );
+			const access_token = jwt.sign(
+				{ id: token.user_id },
+				process.env.ACCESS_TOKEN_SECRET as string,
+				{ expiresIn: process.env.ACCESS_TOKEN_EXPIRATION as string },
+			);
 
-      const tokenID = Math.random().toString(36).slice(2);
-      const refresh_token = jwt.sign(
-        { id: tokenID, user_id: token.user_id },
-        process.env.REFRESH_TOKEN_SECRET as string,
-      );
+			const tokenID = Math.random().toString(36).slice(2);
+			const refresh_token = jwt.sign(
+				{ id: tokenID, user_id: token.user_id },
+				process.env.REFRESH_TOKEN_SECRET as string,
+			);
 
-      await redis.del(`${token.user_id}:${token.id}`);
+			await redis.del(`${token.user_id}:${token.id}`);
 
-      await redis.setEx(
-        `${token.user_id}:${tokenID}` as string,
-        parseInt(process.env.REFRESH_TOKEN_EXPIRATION as string),
-        refresh_token,
-      );
+			await redis.setEx(
+				`${token.user_id}:${tokenID}` as string,
+				parseInt(process.env.REFRESH_TOKEN_EXPIRATION as string),
+				refresh_token,
+			);
 
-      res.json({
-        message: "Success",
-        access_token: access_token,
-        refresh_token: refresh_token,
-      });
-      return;
-    },
-  );
+			return res.json({
+				message: "Success",
+				access_token: access_token,
+				refresh_token: refresh_token,
+			});
+		},
+	);
 }
 
 export async function revokeRefreshToken(req: Request, res: Response) {
-  const keys = await redis.keys(`${req.user.id}:*`);
-  await redis.del(keys);
+	const keys = await redis.keys(`${req.user.id}:*`);
+	await redis.del(keys);
 
-  res.json({ message: "Success" });
-  return;
+	return res.json({ message: "Success" });
 }
--- src/handlers/organizations.ts
+++ src/handlers/organizations.ts
@@ -4,126 +4,115 @@ import { Organization } from "../dbSchemas/organization.js";
 import { resourcifyOrg } from "../utils/resourcify.js";
 import { User } from "../dbSchemas/user.js";
 import type {
-  ICreateOrganization,
-  IInviteUserToOrganization,
-  IUpdateOrganizationByID,
+	ICreateOrganization,
+	IInviteUserToOrganization,
+	IUpdateOrganizationByID,
 } from "../types/handlers/organizations.js";
 
 export async function createOrganization(
-  req: Request<{}, {}, ICreateOrganization>,
-  res: Response,
+	req: Request<{}, {}, ICreateOrganization>,
+	res: Response,
 ) {
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
-    res
-      .status(400)
-      .json({ message: "Validation Errors", errors: result.array() });
-    return;
-  }
+	const result = validationResult(req);
+	if (!result.isEmpty()) {
+		return res
+			.status(400)
+			.json({ message: "Validation Errors", errors: result.array() });
+	}
 
-  const { name, description } = req.body;
+	const { name, description } = req.body;
 
-  const org = new Organization({
-    owner_id: req.user.id,
-    name: name,
-    description: description,
-  });
+	const org = new Organization({
+		owner_id: req.user.id,
+		name: name,
+		description: description,
+	});
 
-  await org.save();
-  res.json({ organization_id: org.id });
-  return;
+	await org.save();
+	return res.json({ organization_id: org.id });
 }
 
 export async function updateOrganizationByID(
-  req: Request<{}, {}, IUpdateOrganizationByID>,
-  res: Response,
+	req: Request<{}, {}, IUpdateOrganizationByID>,
+	res: Response,
 ) {
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
-    res
-      .status(400)
-      .json({ message: "Validation Errors", errors: result.array() });
-    return;
-  }
+	const result = validationResult(req);
+	if (!result.isEmpty()) {
+		return res
+			.status(400)
+			.json({ message: "Validation Errors", errors: result.array() });
+	}
 
-  const { name, description } = req.body;
+	const { name, description } = req.body;
 
-  req.org.name = name;
-  req.org.description = description;
+	req.org.name = name;
+	req.org.description = description;
 
-  await req.org.save();
-  res.json({ message: "Success" });
-  return;
+	await req.org.save();
+	return res.json({ message: "Success" });
 }
 
 export async function deleteOrganizationByID(req: Request, res: Response) {
-  await req.org.deleteOne();
-  res.json({ message: "Success" });
-  return;
+	await req.org.deleteOne();
+	return res.json({ message: "Success" });
 }
 
 export async function getOrganizationByID(req: Request, res: Response) {
-  const org_resource = await resourcifyOrg(req.org);
-  res.json(org_resource);
-  return;
+	const org_resource = await resourcifyOrg(req.org);
+	return res.json(org_resource);
 }
 
 export async function getOrganizations(req: Request, res: Response) {
-  const orgs = (await Organization.find()).filter((org) => {
-    const is_owner: boolean = req.user.id === org.owner_id.toString();
+	const orgs = (await Organization.find()).filter((org) => {
+		const is_owner: boolean = req.user.id === org.owner_id.toString();
 
-    let is_member: boolean = false;
-    org.organization_members.map((member: any) => {
-      if (req.user.id === member.member_id.toString()) is_member = true;
-    });
+		let is_member: boolean = false;
+		org.organization_members.map((member: any) => {
+			if (req.user.id === member.member_id.toString()) is_member = true;
+		});
 
-    if (!is_owner && !is_member) return false;
-    return true;
-  });
+		if (!is_owner && !is_member) return false;
+		return true;
+	});
 
-  const orgs_resources = await Promise.all(
-    orgs.map(async (org) => await resourcifyOrg(org)),
-  );
+	const orgs_resources = await Promise.all(
+		orgs.map(async (org) => await resourcifyOrg(org)),
+	);
 
-  res.json(orgs_resources);
-  return;
+	return res.json(orgs_resources);
 }
 
 export async function inviteUserToOrganization(
-  req: Request<{}, {}, IInviteUserToOrganization>,
-  res: Response,
+	req: Request<{}, {}, IInviteUserToOrganization>,
+	res: Response,
 ) {
-  const result = validationResult(req);
-  if (!result.isEmpty()) {
-    res
-      .status(400)
-      .json({ message: "Validation Errors", errors: result.array() });
-    return;
-  }
+	const result = validationResult(req);
+	if (!result.isEmpty()) {
+		return res
+			.status(400)
+			.json({ message: "Validation Errors", errors: result.array() });
+	}
 
-  const email: string = req.body.user_email;
-  const user: any = await User.findOne({ email: email });
+	const email: string = req.body.user_email;
+	const user: any = await User.findOne({ email: email });
 
-  if (!user) {
-    res.status(404).json({ message: "User Not Found" });
-    return;
-  }
+	if (!user) {
+		return res.status(404).json({ message: "User Not Found" });
+	}
 
-  req.org.organization_members.map((member: any) => {
-    if (user.id === member.member_id) {
-      res.status(403).json({ message: "Already a Member" });
-      return;
-    }
-  });
+	req.org.organization_members.map((member: any) => {
+		if (user.id === member.member_id) {
+			return res.status(403).json({ message: "Already a Member" });
+		}
+	});
 
-  const member = {
-    member_id: user.id,
-    access_level: "A",
-  };
+	const member = {
+		member_id: user.id,
+		access_level: "A",
+	};
 
-  req.org.organization_members.push(member);
+	req.org.organization_members.push(member);
 
-  await req.org.save();
-  res.json({ message: "Success" });
-  return;
+	await req.org.save();
+	return res.json({ message: "Success" });
 }
